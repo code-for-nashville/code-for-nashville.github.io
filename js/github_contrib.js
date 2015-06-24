d866a99d96ac13ee60e408ef2fc3e18931c6f5b7
beck <beck@eventbrite.com>
2015-06-23 21:12:13 -0500

    added default image for members w/o github

--- js/github_contrib.js
+++ js/github_contrib.js
@@ -3,10 +3,15 @@
     var memberImages = $('.member-img');
 
     [].forEach.call(members, function(member, index){
-       $.get('https://api.github.com/users/' + $(member).attr('data-github') + '/events/public',function(data){
-           var avatarImage;
-           avatarImage = data[0].actor.avatar_url;
-           $(memberImages[index]).attr('src', avatarImage);
-       })
+        if($(member).attr('data-github')){
+           $.get('https://api.github.com/users/' + $(member).attr('data-github') + '/events/public',function(data){
+               var avatarImage = '';
+               avatarImage = data[0].actor.avatar_url;
+
+               $(memberImages[index]).attr('src', avatarImage);
+            })
+        } else {
+            $(memberImages[index]).attr('src', '/img/identicon.png');
+        }
     })
 })()
