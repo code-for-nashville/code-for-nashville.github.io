6bbffe2196a9e03dff2e5379f6c291e1eb52e1e6
Tim Eccleston <timothy.perisho.eccleston+git@gmail.com>
2020-01-09 20:02:09 -0600

    simpler way give local images priority over github headshots

--- js/github_contrib.js
+++ js/github_contrib.js
@@ -3,11 +3,11 @@
     var memberImages = $('.member-img');
 
     [].forEach.call(members, function(member, index){
-        if($(member).attr('data-github') && !$(memberImages[index]).attr('src')){
+        if($(member).attr('data-github')){
            $.get('https://api.github.com/users/' + $(member).attr('data-github'),function(data){
                var avatarImage = '';
                avatarImage = data.avatar_url;
-               $(memberImages[index]).attr('src', avatarImage);
+               $(memberImages[index]).attr('src', (index, currentValue) => currentValue || avatarImage);
             })
         }
     })
